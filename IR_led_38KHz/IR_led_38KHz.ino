1a2cb4851de78017bef0dc03922abf78500dd53d
Eize Fokke Hoekstra <s1125464@student.windesheim.nl>
2019-11-20 11:45:23 +0100

    Update IR_led_38KHz.ino
    code aangepast zodat timer nu ook echt 38khz is

--- IR_led_38KHz/IR_led_38KHz.ino
+++ IR_led_38KHz/IR_led_38KHz.ino
@@ -1,5 +1,4 @@
 #include <avr/io.h>
-#include <Arduino.h>
 #include <avr/interrupt.h>
 
 int counter1 = 0;
@@ -7,20 +6,19 @@ int counter2 = 0;
 int positie = 7;
 int sent = 0;
 
-uint8_t bytje = 0b00000000;
-
+uint8_t bytje = 0b111111111;
 
 ISR(TIMER2_COMPA_vect)
 {
   if(~(bytje |~(1 << positie)))
   {
-    if(counter1 < 10)
+    if(counter1 < 1000)
     {
       counter1++;
       PORTD ^= (1<<PORTD3);
     }
     
-    if(counter1 == 10)
+    if(counter1 >= 1000)
     {
       sent = 1;
     }
@@ -31,7 +29,7 @@ ISR(TIMER2_COMPA_vect)
       PORTD &= ~(1<<PORTD3);
     }
     
-    if(counter2 == 10)
+    if(counter2 >= 1000)
     {
       counter1 = 0;
       counter2 = 0;
@@ -40,27 +38,27 @@ ISR(TIMER2_COMPA_vect)
     }
   }else
   {
-    if(counter1 < 10)
+    if(counter1 < 1000)
     {
       counter1++;
       PORTD ^= (1<<PORTD3);
     }
     
-    if(counter1 == 10)
+    if(counter1 >= 1000)
     {
       sent = 1;
     }
-    
+  
     if(sent)
     {
       counter2++;
       PORTD &= ~(1<<PORTD3);
     }
     
-    if(counter2 == 30)
+    if(counter2 >= 3000)
     {
-      counter2 = 0;
       counter1 = 0;
+      counter2 = 0;
       positie--;
       sent = 0;
     }
@@ -72,12 +70,12 @@ ISR(TIMER2_COMPA_vect)
   }
 }
 
+
 void timer2_setup();
 void IR_led_setup();
 
 int main()
 {
-  init();
   timer2_setup();
   IR_led_setup();
   sei();
@@ -94,17 +92,17 @@ void IR_led_setup()
 
 void timer2_setup()
 {
-  // TIMER 2 for interrupt frequency 37735.84905660377 Hz:
   cli(); // stop interrupts
   TCCR2A = 0; // set entire TCCR2A register to 0
   TCCR2B = 0; // same for TCCR2B
   TCNT2  = 0; // initialize counter value to 0
-  // set compare match register for 37735.84905660377 Hz increments
-  OCR2A = 52; // = 16000000 / (8 * 37735.84905660377) - 1 (must be <256)
+  // set compare match register for 761904.7619047619 Hz increments
+  OCR2A = 52; // = 16000000 / (1 * 37000) - 1 (must be <256)
   // turn on CTC mode
-  TCCR2B |= (1 << WGM21);
-  // Set CS22, CS21 and CS20 bits for 8 prescaler
-  TCCR2B |= (1 << CS22) | (1 << CS21) | (0 << CS20);
+  TCCR2A |= (0<<WGM20)|(1 << WGM21);
+  TCCR2B |= (0<<WGM22);
+  // Set CS22, CS21 and CS20 bits for 1 prescaler
+  TCCR2B |= (0 << CS22) | (1 << CS21) | (0 << CS20);
   // enable timer compare interrupt
   TIMSK2 |= (1 << OCIE2A);
 }
