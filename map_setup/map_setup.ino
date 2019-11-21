5eb785a0e688a508688aa400c36add9f66874b09
iYannick0 <43132788+iYannick0@users.noreply.github.com>
2019-11-21 19:35:22 +0100

    Create map_setup.ino
    yehees, map werkt
    
    "ziet er prachtig uit"
    -Theo Hiddema(2019)

---
+++ map_setup/map_setup.ino
@@ -0,0 +1,42 @@
+#include <Adafruit_GFX.h>         // Core graphics library
+#include <Adafruit_ILI9341.h>     // Hardware-specific library
+#include <SdFat.h>                // SD card & FAT filesystem library
+#include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
+#include <Adafruit_ImageReader.h> // Image-reading functions
+
+#define USE_SD_CARD
+
+// TFT display and SD card share the hardware SPI interface, using
+// 'select' pins for each to identify the active device on the bus.
+
+#define SD_CS   4 // SD card select pin
+#define TFT_CS 10 // TFT select pin
+#define TFT_DC  9 // TFT display/command pin
+
+SdFat                SD;         // SD card filesystem
+Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
+
+
+Adafruit_ILI9341     tft    = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+
+void setup(void)
+{
+  ImageReturnCode stat; // Status from image-reading functions
+
+  tft.begin();          // Initialize screen
+  SD.begin(SD_CS, SD_SCK_MHZ(25));
+
+  // Fill screen blue. Not a required step, this just shows that we're
+  // successfully communicating with the screen.
+  tft.fillScreen(ILI9341_BLUE);
+
+  // Load full-screen BMP file 'map.bmp' at position (0,0) (top left).
+  // Notice the 'reader' object performs this, with 'tft' as an argument.
+  
+  stat = reader.drawBMP("/map.bmp", tft, 0, 0);
+}
+
+void loop() 
+{
+}
