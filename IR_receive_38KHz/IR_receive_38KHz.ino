2574681f1c3abe8cbd623566ea77a7fff98cec3d
Eize Fokke Hoekstra <s1125464@student.windesheim.nl>
2019-11-21 13:50:36 +0100

    Update IR_receive_38KHz.ino
    ir receive

--- IR_receive_38KHz/IR_receive_38KHz.ino
+++ IR_receive_38KHz/IR_receive_38KHz.ino
@@ -2,41 +2,66 @@
 #include <Arduino.h>
 #include <avr/interrupt.h>
 
-int counter = 0;
-int prev_counter = 0;
-int current_counter = 0;
+int counter;
+int prev_counter;
+int current_counter;
 int difference_counters = 0;
-unsigned int state;
-
+int count_interrupts = 0;
+int bit_positie = 7;
 int falling_edge = 1;
 
+uint8_t data_byte;
+
 void timer2_setup();
+void PCINT1_setup();
+void setup_pin3();
 
 ISR(TIMER2_COMPA_vect)
 {
   counter++;
-  //Serial.println(counter);
 }
 
 ISR(INT1_vect)
 {
+  count_interrupts++;
   EICRA ^= (1<<ISC10);
   if(falling_edge)
   {
     prev_counter = counter;
     falling_edge = 0;
-  }else if(falling_edge == 0)
+  }else
   {
     current_counter = counter;
     falling_edge = 1;
-    counter = 0;
     difference_counters = current_counter - prev_counter;
+    counter = 0;
+  }
+
+  if(bit_positie < 0)
+  {
+    bit_positie = 7;
+    data_byte = 0;
+  }
+    
+  if(difference_counters >= 290 && difference_counters <= 390 && (count_interrupts%2 == 0)) //is 1
+  {
+    data_byte |= (1<<bit_positie);
+    count_interrupts = 0;
+    bit_positie--;
+    Serial.print(1);
+  }else if(difference_counters <= 200 && difference_counters >= 100 && (count_interrupts%2==0)) //is 0
+  {
+    data_byte &= ~(1<<bit_positie);
+    count_interrupts = 0;
+    bit_positie--;
+    Serial.print(0);
   }
 }
 
 int main()
 {
   init();
+  Serial.begin(38400);
   setup_pin3();
   timer2_setup();
   PCINT1_setup();
@@ -64,7 +89,7 @@ void timer2_setup()
   // turn on CTC mode
   TCCR2A |= (1 << WGM21);
   // Set CS22, CS21 and CS20 bits for 8 prescaler
-  TCCR2B |= (1 << CS22) | (1 << CS21) | (0 << CS20);
+  TCCR2B |= (0 << CS22) | (1 << CS21) | (0 << CS20);
   // enable timer compare interrupt
   TIMSK2 |= (1 << OCIE2A);
 }
