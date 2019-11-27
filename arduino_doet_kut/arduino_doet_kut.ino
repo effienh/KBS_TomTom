36c3d1f5d76df07e6159ac23554758ecc67ec336
Eize Fokke Hoekstra <s1125464@student.windesheim.nl>
2019-11-27 14:23:29 +0100

    Update arduino_doet_kut.ino
    correct

--- arduino_doet_kut/arduino_doet_kut.ino
+++ arduino_doet_kut/arduino_doet_kut.ino
@@ -1,42 +1,30 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <Nunchuk.h>
+#include <Wire.h>
+
 #include <Adafruit_GFX.h>         // Core graphics library
 #include <Adafruit_ILI9341.h>     // Hardware-specific library
-#include <SdFat.h>                // SD card & FAT filesystem library
 #include <Adafruit_SPIFlash.h>    // SPI / QSPI flash library
 #include <Adafruit_ImageReader.h> // Image-reading functions
+#include <Adafruit_EPD.h>
+
+#include <MinimumSerial.h>
+#include <SdFat.h>
 
 #include <Adafruit_SPITFT.h>
 #include <Adafruit_SPITFT_Macros.h>
 #include <gfxfont.h>
 
-#include <avr/io.h>
-#include <Nunchuk.h>
-#include <Wire.h>
-#include <avr/interrupt.h>
-
 #define USE_SD_CARD
 
-int BORDER_UP = 180;
-int BORDER_DOWN = 17;
-int BORDER_LEFT = 86;
-int BORDER_RIGHT = 250;
+int BORDER_UP = 176;
+int BORDER_DOWN = 48;
+int BORDER_LEFT = 102;
+int BORDER_RIGHT = 246;
 
-// TFT display and SD card share the hardware SPI interface, using
-// 'select' pins for each to identify the active device on the bus.
-
-#define SD_CS   4 // SD card select pin
-#define TFT_CS 10 // TFT select pin
-#define TFT_DC  9 // TFT display/command pin
-  
-SdFat SD;         // SD card filesystem
-Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
-
-
-Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
-
-void timer2_setup();
-
-int y_waarde = 128;
-int x_waarde = 128;
+int y_waarde = 208;
+int x_waarde = 116;
 
 int deadzone = 0;
 int prev_state = 0;
@@ -51,16 +39,23 @@ int onder;
 
 int pixel = 16;
 
-void setupWire()
-{
-  Wire.begin();
-  init();
-  nunchuk_init();
-}
+// TFT display and SD card share the hardware SPI interface, using
+// 'select' pins for each to identify the active device on the bus.
+
+#define SD_CS   4 // SD card select pin
+#define TFT_CS 10 // TFT select pin
+#define TFT_DC  9 // TFT display/command pin
+  
+SdFat SD;         // SD card filesystem
+Adafruit_ImageReader reader(SD); // Image-reader object, pass in SD filesys
+
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+
+void timer1_setup();
+void setupWire();
 
 ISR(TIMER1_COMPA_vect)
 {
-  //Serial.println("neger");
    if(nunchuk_joystickY_raw() > 200) //up
    {
     up = 1;
@@ -81,48 +76,26 @@ ISR(TIMER1_COMPA_vect)
 
 void setup(void)
 {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   ImageReturnCode stat; // Status from image-reading functions
   
   tft.begin();          // Initialize screen
   SD.begin(SD_CS, SD_SCK_MHZ(25));
   setupWire();
-  timer2_setup();
-  
-
-  // Fill screen blue. Not a required step, this just shows that we're
-  // successfully communicating with the screen
-  
-  // Load full-screen BMP file 'map.bmp' at position (0,0) (top left).
-  // Notice the 'reader' object performs this, with 'tft' as an argument.
+  timer1_setup();
   
   stat = reader.drawBMP("/map.bmp", tft, 0, 0);
 }
 
 void loop() 
 {
-  
   if (nunchuk_read()) 
       {
-        /*
-          if((nunchuk_joystickY_raw() == deadzone) && (nunchuk_joystickX_raw() == deadzone))
-          {
-            prev_state = 0;
-          }
-          */
-
-        //if(prev_state == 0)
-        //{
-          
-          //if(nunchuk_joystickY_raw() > 200) //up
           if(up)
           {
             y_waarde = y_waarde + pixel;
             tft.fillRect(y_waarde, x_waarde, pixel, pixel, 0x0000);
-            //tft.fillRect((y_waarde - 5), x_waarde, 5, 5, 0xFFFF);
             ImageReturnCode ground_refresh = reader.drawBMP("/blok.bmp", tft, y_waarde - pixel, x_waarde);
-            
-            //prev_state = 1;
             
             if(y_waarde > BORDER_UP)
             {
@@ -130,33 +103,26 @@ void loop()
             }
             up = 0;
           }
-
-          //if(nunchuk_joystickX_raw() > 200) //right
+          
           if(rechts)
           {
             x_waarde = x_waarde + pixel;
             tft.fillRect(y_waarde, x_waarde, pixel, pixel, 0x0000);
-            //tft.fillRect(y_waarde, (x_waarde - 5), pixel, pixel, 0xFFFF);
             ImageReturnCode ground_refresh = reader.drawBMP("/blok.bmp", tft, y_waarde, x_waarde - pixel);
 
-            //prev_state = 1;
-            
             if(x_waarde > BORDER_RIGHT)
             {
               x_waarde = BORDER_RIGHT;
             }
             rechts = 0;
           }
-
-          //if(nunchuk_joystickX_raw() < 50) //left
+          
           if(links)
           {
             x_waarde = x_waarde - pixel;
             tft.fillRect(y_waarde, x_waarde, pixel, pixel, 0x0000);
             //tft.fillRect(y_waarde, (x_waarde + 5), 5, 5, 0xFFFF);
             ImageReturnCode ground_refresh = reader.drawBMP("/blok.bmp", tft, y_waarde, x_waarde + pixel);
-
-            //prev_state = 1;
 
             if(x_waarde < BORDER_LEFT)
             {
@@ -165,15 +131,11 @@ void loop()
             links = 0;
           }
 
-          //if(nunchuk_joystickY_raw() < 50) //down
           if(onder)
           {
             y_waarde = y_waarde - pixel;
             tft.fillRect(y_waarde, x_waarde, pixel, pixel, 0x0000);
-            //tft.fillRect((y_waarde + 5), x_waarde, 5, 5, 0xFFFF);
             ImageReturnCode ground_refresh = reader.drawBMP("/blok.bmp", tft, y_waarde + pixel, x_waarde);
-
-            //prev_state = 1;
 
             if(y_waarde < BORDER_DOWN)
             {
@@ -196,22 +158,28 @@ void loop()
           }*/
         //}
       }
-    _delay_ms(120);
 }
 
-void timer2_setup()
+void timer1_setup()
 {
-  // TIMER 2 for interrupt frequency 37735.84905660377 Hz:
   cli(); // stop interrupts
-  TCCR1A = 0; // set entire TCCR2A register to 0
-  TCCR1B = 0; // same for TCCR2B
+  TCCR1A = 0; // set entire TCCR1A register to 0
+  TCCR1B = 0; // same for TCCR1B
   TCNT1  = 0; // initialize counter value to 0
-  // set compare match register for 37735.84905660377 Hz increments
-  OCR1B = 52; // = 16000000 / (8 * 37735.84905660377) - 1 (must be <256)
+  // set compare match register for 120.00480019200768 Hz increments
+  OCR1A = 12000; // = 16000000 / (8 * 120.00480019200768) - 1 (must be <65536)
   // turn on CTC mode
-  TCCR1A |= (1 << WGM11);
-  // Set CS22, CS21 and CS20 bits for 8 prescaler
-  TCCR1B |= (0 << CS12) | (1 << CS11) | (0 << CS10);
+  TCCR1B |= (1 << WGM12);
+  // Set CS12, CS11 and CS10 bits for 8 prescaler
+  TCCR1B |= (1 << CS12) | (0 << CS11) | (0 << CS10);
   // enable timer compare interrupt
   TIMSK1 |= (1 << OCIE1A);
+  sei(); // allow interrupts
+}
+
+void setupWire()
+{
+  Wire.begin();
+  init();
+  nunchuk_init();
 }
