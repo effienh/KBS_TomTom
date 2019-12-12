7ccda579475348c11814b185ff922576bd1c440d
Eize Fokke Hoekstra <s1125464@student.windesheim.nl>
2019-12-12 13:52:54 +0100

    heilige code
    Receive en zenden tegelijk op twee arduino's

--- Led38Khz_Receive56Khz/Led38Khz_Receive56Khz.ino
+++ Led38Khz_Receive56Khz/Led38Khz_Receive56Khz.ino
@@ -12,9 +12,21 @@ int bit_positie = 7;
 int falling_edge = 1;
 
 int data_correct = 0;
-int data_byte[9];
 
 int middle, up, down, left, right, buttonc;
+
+int counter1 = 0;
+int counter2 = 0;
+int positie = 7;
+int sent = 0;
+
+uint8_t bytje = 0;
+
+void timer2_setup();
+void IR_led_setup();
+void setupWireNunchuk();
+void nunchuk_send_byte();
+void check_buttonZ();
 
 void timer1_setup();
 void PCINT1_setup();
@@ -23,6 +35,68 @@ void setup_pin3();
 ISR(TIMER1_COMPA_vect)
 {
   counter++;
+}
+
+ISR(TIMER2_COMPA_vect)
+{
+  if (~(bytje | ~(1 << positie)))
+  {
+    if (counter1 < 100 /*&& !nunchuk_middle*/)
+    {
+      counter1++;
+      PORTD ^= (1 << PORTD6);
+    }
+
+    if (counter1 >= 100)
+    {
+      sent = 1;
+    }
+
+    if (sent)
+    {
+      counter2++;
+      PORTD &= ~(1 << PORTD6);
+    }
+
+    if (counter2 >= 100)
+    {
+      counter1 = 0;
+      counter2 = 0;
+      positie--;
+      sent = 0;
+    }
+  } else
+  {
+    if (counter1 < 100 /*&& !nunchuk_middle*/)
+    {
+      counter1++;
+      PORTD ^= (1 << PORTD6);
+    }
+
+    if (counter1 >= 100)
+    {
+      sent = 1;
+    }
+
+    if (sent)
+    {
+      counter2++;
+      PORTD &= ~(1 << PORTD6);
+    }
+
+    if (counter2 >= 300)
+    {
+      counter1 = 0;
+      counter2 = 0;
+      positie--;
+      sent = 0;
+    }
+  }
+
+  if (positie == -1)
+  {
+    positie = 7;
+  }
 }
 
 ISR(INT1_vect)
@@ -81,15 +155,22 @@ ISR(INT1_vect)
 
 int main()
 {
-  init();
-  Serial.begin(9600);
+  setupWireNunchuk();
+  IR_led_setup();
   setup_pin3();
   timer1_setup();
+  timer2_setup();
   PCINT1_setup();
   sei();
 
   while (1)
   {
+    if (nunchuk_read())
+    {
+      nunchuk_send_byte();
+    }
+    check_buttonZ();
+
     if (middle > 3)
     {
       Serial.println("MIDDLE");
@@ -128,6 +209,11 @@ void setup_pin3()
   DDRD &= ~(1 << DDD3);
 }
 
+void IR_led_setup()
+{
+  DDRD |= (1 << DDD6);
+}
+
 void timer1_setup()
 {
   cli(); // stop interrupts
@@ -144,9 +230,62 @@ void timer1_setup()
   TIMSK1 |= (1 << OCIE1A);
 }
 
+void timer2_setup()
+{
+  cli(); // stop interrupts
+  TCCR2A = 0; // set entire TCCR2A register to 0
+  TCCR2B = 0; // same for TCCR2B
+  TCNT2  = 0; // initialize counter value to 0
+  // set compare match register for 761904.7619047619 Hz increments
+  OCR2A = 52; // = 16000000 / (1 * 37000) - 1 (must be <256)
+  // turn on CTC mode
+  TCCR2A |= (0 << WGM20) | (1 << WGM21);
+  TCCR2B |= (0 << WGM22);
+  // Set CS22, CS21 and CS20 bits for 1 prescaler
+  TCCR2B |= (0 << CS22) | (1 << CS21) | (0 << CS20);
+  // enable timer compare interrupt
+  TIMSK2 |= (1 << OCIE2A);
+}
+
 void PCINT1_setup()
 {
   EIMSK |= (1 << INT1);
   EICRA |= (1 << ISC11);
   EICRA |= (1 << ISC10);
+}
+
+void setupWireNunchuk()
+{
+  init();
+  Wire.begin(0x52);
+  Serial.begin(9600);
+  nunchuk_init();
+}
+
+void nunchuk_send_byte()
+{
+  if (nunchuk_joystickY_raw() > 200) // up
+  {
+    bytje = 0b00000001;
+  } else if (nunchuk_joystickY_raw() < 55) //down
+  {
+    bytje = 0b00000011;
+  } else if ((nunchuk_joystickY_raw() < 135 && nunchuk_joystickY_raw() > 120) && (nunchuk_joystickX_raw() < 135 && nunchuk_joystickX_raw() > 120))
+  {
+    bytje = 0b00011111;
+  } else if (nunchuk_joystickX_raw() < 55) //links
+  {
+    bytje = 0b00000111;
+  } else if (nunchuk_joystickX_raw() > 200)//rechts
+  {
+    bytje = 0b00001111;
+  }
+}
+
+void check_buttonZ()
+{
+  if(nunchuk_buttonC())
+  {
+    bytje = 0b00000000;
+  }
 }
